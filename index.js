91961e388673c1c296ae9560e413e4c0d996c464
Kevin <staffordkevinjames@gmail.com>
2023-09-25 16:02:09 -0400

    ADD: fetchResource function

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const theMetAPI = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
-const parseJSON  = (resp) => resp.json()
+const randomPageNum = Math.floor(Math.random() * 10)
+const breweryAPI = `https://api.openbrewerydb.org/v1/breweries?${randomPageNum}&per_page=10`
+const galleryDiv = document.getElementById('')
 
-
+// Function to fetch the brewery data from api
 function fetchResource(url) {
     return fetch(url)
-    .then(parseJSON)
+    .then((r) => r.json())
+    .then(breweryData => {
+        breweryData.forEach(renderBreweryGallery)
+        
+    })
 }
 
